921b58feba0c41356db3da6b1320e9a23eedfc0d
Arzu Suleymanov <arzu.suleamanov@firstlinesoftware.com>
2019-08-12 14:00:44 +0200

    Add education and experiences functionality

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,7 +1,8 @@
 import {
     GET_PROFILE,
     PROFILE_ERROR,
-    CLEAR_PROFILE
+    CLEAR_PROFILE,
+    UPDATE_PROFILE
 } from "../actions/types";
 
 const initialState = {
@@ -20,6 +21,7 @@ export default function (state = initialState, action) {
 
     switch (type) {
         case GET_PROFILE:
+        case UPDATE_PROFILE:
             return {
                 ...state,
                 profile: payload,
@@ -35,7 +37,7 @@ export default function (state = initialState, action) {
             return {
                 ...state,
                 profile: null,
-                repos:[],
+                repos: [],
                 loading: false
             }
         default:
